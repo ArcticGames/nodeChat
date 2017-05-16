407b7e9adeda2bc0203570433d880aaa87ad505a
leoworm <leo.charles.p@gmail.com>
2017-05-16 20:24:13 +0200

    Working chat

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static(__dirname + '/public'));
+
 const port = 3000;
 
 server.listen(port, function(){
   console.log('Server listening on port: ' + port);
 });
 
+app.get('')
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client.html');
 });
+
+io.on('connection', function(socket){
+  console.log('A user has joined. His ID is: ' + socket.id)
+  socket.emit('event', {message:'A user has joined'});
+  socket.on('chatMsg', function(msg){
+    socket.emit('chatMsg', msg);
+    console.log('Message: ' + msg);
+  })
+})
